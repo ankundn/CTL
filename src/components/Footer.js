d8f16592c3427e01e3e4aa2d4284f7b693908ed5
Ankunda Nisha <ankundanisha0@gmail.com>
2020-08-26 12:18:44 +0300

    Added footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <h1>This is footer</h1>
+    <div className="footer">
+      <h1>Corporate Technical Services</h1>
+      <h1>ghjjhjjjnj</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -18,13 +18,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  Route,
-  Link,
-  NavLink,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 import About from "./components/About";
