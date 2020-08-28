4b53dbb5541a0e327889e5a6c9db76111834c8be
Ankunda Nisha <ankundanisha0@gmail.com>
2020-08-28 10:31:04 +0300

    Worked on the header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { MDBAnimation } from "mdbreact";
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <MDBAnimation type="zoomIn" duration="3s" delay="600ms">
+          <img
+            className="img-fluid"
+            alt=""
+            src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png"
+          />
+        </MDBAnimation>
         <h1>Main Page</h1>
       </div>
     );
---
+++ src/components/headeroriginal.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// function Header() {
+//   return (
+//     <nav className="header">
+//       <NavLink className="headername" exact activeClassName="active" to="/">
+//         Home
+//       </NavLink>
+//       <NavLink className="headername" activeClassName="active" to="/about">
+//         About
+//       </NavLink>
+//       <NavLink className="headername" activeClassName="active" to="/contact">
+//         Contact
+//       </NavLink>
+//     </nav>
+//   );
+// }
+// export default Header;
