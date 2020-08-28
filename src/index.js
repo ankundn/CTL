0e25f84b2ba265c950ffcb9e22cb892f1c2de787
Ankunda Nisha <ankundanisha0@gmail.com>
2020-08-28 09:18:47 +0300

    installed mdb react

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
