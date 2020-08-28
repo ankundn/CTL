0eb1240da2a1e3a6e716795f45c92bf3aeb347fa
Ankunda Nisha <ankundanisha0@gmail.com>
2020-08-28 12:43:13 +0300

    renamed <p> style to whiteSpace from white-space

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,3 @@
-// import React from "react";
-
-// function Footer() {
-//   return (
-//     <div className="footer">
-//       <h1>
-//         {"Corporate Technical Services a Group Company of Captiva Africa"}
-//       </h1>
-
-//       <h3>Contact Us: </h3>
-//       <p
-//         style={{
-//           whiteSpace: "pre-wrap",
-//         }}
-//       >
-//         {
-//           "8 Marisa Court Lavington \n P.O Box 6389-200 \n Nairobi +254 722 329 357 \n info@cts.co.ke \n info@captivaafrica.com"
-//         }
-//       </p>
-//       <p
-//         style={{
-//           whiteSpace: "pre-wrap",
-//         }}
-//       >
-//         {"1 Bukerere Road, Setta \n Kampala Uganda \n+256794094966"}
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default Footer;
-
-// // const styles = {
-// //   fontFamily: 'sans-serif',
-// //   textAlign: 'center',
-// // };
-
-// // const App = () => {
-// //   return (
-// //   <div style={styles}>
-// //     <Hello name="CodeSandbox" />
-// //     <p style={{
-// //       'white-space': 'pre-wrap'
-// //       }}>{"This \n works"}</p>
-// //     <p style={{
-// //       'white-space': 'pre-wrap'
-// //     }}>Does \n not \n work</p>
-// //   </div>
-// //   );
-// // };
-
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
@@ -61,7 +10,7 @@ const Footer = () => {
             <h5 className="title">Corporate Technical Services</h5>
             <p
               style={{
-                "white-space": "pre-wrap",
+                whiteSpace: "pre-wrap",
               }}
             >
               {
@@ -70,7 +19,7 @@ const Footer = () => {
             </p>
             <p
               style={{
-                "white-space": "pre-wrap",
+                whiteSpace: "pre-wrap",
               }}
             >
               {"1 Bukerere Road, Setta\n Kampala, Uganda \n +256-794-094-966 "}
