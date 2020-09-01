6b8269180c84999661a019d6cb957126dbd9fefb
Ankunda Nisha <ankundanisha0@gmail.com>
2020-09-01 12:31:52 +0300

    Added more content to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ class About extends React.Component {
           Our initial services comprised the automation and systemintegration of
           traditional security monitoring and response options.
         </p>
-        <p></p>
+
         <p
           style={{
             whiteSpace: "pre-wrap",
@@ -27,6 +27,36 @@ class About extends React.Component {
             " These included the design, installation and maintenance of electronic surveillance products such as Close Circuit Television(CCTV) systems,\nand multi-redundancy counter-intrusion systems for equipment and  premises "
           }
         </p>
+        <div>
+          <h4>Milestones</h4>
+          <p>
+            Some of our milestones in the evolution of the security sector in
+            the region include the following:
+          </p>
+          <ul>
+            <li>
+              CTS was the first company in EA to offer work-flow solutions to
+              the security decision chain for corporates
+            </li>
+            <li>
+              CTS pioneered the use of Radio Frequency tag technology for large
+              stores and supermarkets in Kenya to help prevent losses from theft
+              by employees and customers
+            </li>
+            <li>
+              CTS was the first local private company in East Africa to be
+              contracted by government to offer long term technical assistance
+              to the police and ministry of interior
+            </li>
+          </ul>
+          <p>
+            Our expansion into provision of advisory services was a natural
+            progression from security hardware and system integration. This was
+            on a large part in response to the evolving challenges faced by the
+            corporate client who encountered far more risk of exposure to loss
+            from internal factors, rather than external threats.
+          </p>
+        </div>
       </div>
     );
   }
