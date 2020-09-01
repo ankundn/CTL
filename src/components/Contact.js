b4fa6250f21f7a01659039894753b263366e7023
Ankunda Nisha <ankundanisha0@gmail.com>
2020-09-01 13:55:31 +0300

    Added content to the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,22 @@ import "../index.css";
 const FormPage = () => {
   return (
     <MDBContainer fluid>
+      <div>
+        <h2>Our Team</h2>
+        <p>
+          Our client relationship management philosophy is simple: establish
+          partnerships, provide responsive service, understand client goals,
+          apply intelligent solutions and treat clients with respect. Rather
+          than considering CTS as a vendor of services, we ask that our clients
+          view us as an extension of their teams.
+        </p>
+        <p>
+          Within CTS our clients have access to a team of highly committed and
+          skilled professionals with a range of skills and experience in the
+          security, investigations and executive management in both public and
+          private sectors.
+        </p>
+      </div>
       <MDBRow className="contactform">
         <MDBCol md="5">
           <form>
