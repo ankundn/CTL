43d3a05a5cf691ad0ec031f060089397f3902a27
Ankunda Nisha <ankundanisha0@gmail.com>
2020-09-01 13:38:43 +0300

    Added content to the service page

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,7 +2,70 @@ import React from "react";
 
 class Service extends React.Component {
   render() {
-    return <h1>Here is what we do</h1>;
+    return (
+      <div>
+        <h1>Client Service</h1>
+        <p>
+          We have since phased out of the supply and system integration of
+          security solutions to focus on investigations and advisory services.
+        </p>
+        <h3>Our advisory and investigation services include:</h3>
+        <ul>
+          <li>Intellectual Property Violation Investigations</li>
+          <li>Executive Due Diligence</li>
+          <li>Pre-Investment Due Diligence</li>
+          <li>Corporate Fraud and Theft Investigation</li>
+          <li>Litigation Support</li>
+          <li>
+            Business risk mitigation ( due diligence on supply chain partners)
+          </li>
+          <li>
+            Board advisory ( screening of directors of boards) in mergers and
+            acquisitions
+          </li>
+        </ul>
+        <h3>Executive Due Diligence</h3>
+        <p>
+          From our experience, the majority of corporate crime does not
+          originate from the shop floor but from the boardroom. For example a
+          number of companies in Kenya and the region have lately been caught up
+          in adverse publicity on account of their senior staff being involved
+          in a variety of highly publicised scandals. Candidates for executive
+          positions often have long job histories spanning several companies and
+          geographies, so much so that a standard background check is at times
+          not enough. Executive due diligence is therefore a worthwhile
+          investment of resources, compared to what is at stake. We conduct due
+          diligence, on behalf of clients, on executive level staff or
+          candidates, as well as on key persons at prospective investee
+          companies or partners. Our executive due diligence investigations may
+          be tailored to cover the following areas:
+        </p>
+        <ul>
+          <li>Confirmation of employment history</li>
+          <li>
+            Confirmation of academic and professional license qualifications
+          </li>
+          <li>History of criminal and civil litigation</li>
+          <li>Misrepresentations in their backgrounds</li>
+          <li>Reputation Risks</li>
+          <li>Politically exposed persons (PEPs)</li>
+          <li> Client/supplier relationships</li>
+          <li>Extensive reference interviews</li>
+          <li>Interpol search</li>
+          <li>Source of Wealth</li>
+          <li>Conflict of Interest</li>
+          <li>Pending Litigation</li>
+          <li>Involvement in other businesses</li>
+          <li>Credit Bureau Reference Checks</li>
+          <li>Links to Organized Crime</li>
+          <li>Verification of Credentials</li>
+          <li>Debts and Solvency</li>
+          <li>Judgments</li>
+          <li>Regulatory Violations</li>
+        </ul>
+        <p>Any other information of interest to the client</p>
+      </div>
+    );
   }
 }
 
