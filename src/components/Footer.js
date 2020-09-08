27e72b039c2490be8c898e5a94c1b54ecd3ee6f1
ankundn <ankundnisha0@gmail.com>
2020-09-08 12:20:14 +0300

    Worked on the contents in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import "../index.css";
 
 const Footer = () => {
   return (
     <MDBFooter color="blue" className="font-small pt-4 mt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
-          <MDBCol md="6">
+          <MDBCol md="6"  className="footeraddress">
             <h5 className="title">Corporate Technical Services</h5>
             <p
               style={{
                 whiteSpace: "pre-wrap",
               }}
+             
             >
               {
                 "8 Marissa Court Lavington\n P.O Box 6389-200 \n Nairobi, Kenya \n +254-722-329-357 \n info@cts.co.ke \n info@captivaafrica.com"
@@ -21,33 +23,20 @@ const Footer = () => {
               style={{
                 whiteSpace: "pre-wrap",
               }}
+              
             >
               {"1 Bukerere Road, Setta\n Kampala, Uganda \n +256-794-094-966 "}
             </p>
+            
           </MDBCol>
 
-          {/* <MDBCol md="6">
-            <h5 className="title">For More Information</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="info@cts.co.ke">Link 1</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-          </MDBCol> */}
+          
+          
         </MDBRow>
       </MDBContainer>
       <div className="footer-copyright text-right py-3">
         <MDBContainer fluid>
-          Copyright:{" Ankunda Nisha Stephanie"}
+          &copy; {"Corporate Technical Services 2020"}
         </MDBContainer>
       </div>
     </MDBFooter>
