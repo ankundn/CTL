b698e86f751469793a98fbedb76d6d58e362545e
ankundn <ankundnisha0@gmail.com>
2020-09-21 22:15:27 +0300

    Installed react bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
 import 'semantic-ui-css/semantic.min.css' 
