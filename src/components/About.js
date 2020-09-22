a0c56f51fe9937d93280a61fa588a500801c8e04
ankundn <ankundnisha0@gmail.com>
2020-09-22 16:49:27 +0300

    Added a jumbotron, going to add a grid of three colums below

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../index.css";
 // import Footer from "../components/Footer";
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 class About extends React.Component {
   render() {
@@ -64,30 +64,43 @@ class About extends React.Component {
       //   </div>
       //   <Footer />
       // </div>
-
-      
-<Jumbotron>
-  <h1 className="aboutheader">Corporate Technical Services</h1>
-   <p className="aboutcontent">
-            A subsidiary of Captiva Africa Ltd
+<div>
+      <MDBJumbotron fluid className="jumbo">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol>
+          <h2 className="h1 display-4 text-center">Corporate Technical Services</h2>
+            <p className="aboutcontent">
+           A subsidiary of Captiva Africa Ltd
            (<a href="https://captivaafrica.com">Captiva Africa</a>), was
-           established in 2002 in Nairobi,Kenya. 
-        </p>
-           <p className="aboutcontent">
-           When we started off, our mision was to integrate innovate technology
-           in the provision of security solutions for the corporate client.
-         </p>
-         <p className="aboutcontent">
+           established in 2002 in Nairobi,Kenya
+            </p>
+            <p className="aboutcontent">
+              When we started off, our mision was to integrate innovate technology
+        in the provision of security solutions for the corporate client.
+            </p>
+            <p className="aboutcontent">
            Our initial services comprised the automation and system integration of
            traditional security monitoring and response options.
         </p>
-  {/* <p>
-    <Button variant="primary">Learn more</Button>
-  </p> */}
+            {/* <hr className="my-2" /> */}
+            {/* <p>
+              It uses utility classes for typgraphy and spacing to space content out
+              within the larger container.
+            </p> */}
+            <div className="text-center">
+              <MDBBtn color="primary" href="/service">CTS SERVICES</MDBBtn>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </MDBJumbotron>
   
-</Jumbotron>
+  
+  
 
-      
+  
+    </div>
     );
   }
 }
