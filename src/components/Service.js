d38e9f6a05dc7316f20a1edda5a08763d3da4c63
ankundn <ankundnisha0@gmail.com>
2020-09-28 18:07:36 +0300

    changed class to className

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,7 +2,7 @@
 // import "../index.css";
 // import Footer from "../components/Footer"
 
-// class Service extends React.Component {
+// className Service extends React.Component {
 //   render() {
 //     return (
 //       <div>
@@ -95,8 +95,8 @@ class Service extends Component  {
       
       <div>
   
-    <h3 class="font-weight-bold text-center dark-grey-text pb-2">Our Services</h3>
-    <hr />
+    <h1 className="font-weight-bold text-center dark-grey-text pb-2">Our Services</h1>
+    {/* <hr /> */}
        <p className="servicecontent">
              Our expansion into provision of advisory services was a natural
              progression from security hardware and system integration.
@@ -105,38 +105,38 @@ class Service extends Component  {
 
 
         <div className="servicerow"> 
-<div class="row">
+<div className="row">
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center bg-success text-white">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="far fa-object-ungroup fa-4x"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="text-white">Intellectual Property Violation Investigations</a></h5>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center bg-success text-white">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="far fa-object-ungroup fa-4x"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Intellectual Property Violation Investigations</h5>
       
-      <p class="mb-4">Short explanation on IP
+      <p className="mb-4">Short explanation on IP
       </p>
     
     </div>
   </div>
 </div>
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="fas fa-mobile-alt fa-4x grey-text"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="dark-grey-text" href="/executiveduediligence">Executive Due Dilligence</a></h5>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="fas fa-mobile-alt fa-4x grey-text"></i></p>
+      <h5 className="font-weight-normal my-4 py-2"><a className="dark-grey-text" href="/executiveduediligence">Executive Due Dilligence</a></h5>
       
-      <p class="text-muted mb-4">Click for more information</p>
+      <p className="text-muted mb-4">Click for more information</p>
     </div>
   </div>
 </div>
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center deep-purple lighten-1 text-white">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="fas fa-chart-line fa-4x"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="text-white" >Pre-Investment Due Diligence</a></h5>
-      <p class="mb-4">Short Explanation on Pre-Investment Due Dilligence</p>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center deep-purple lighten-1 text-white">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="fas fa-chart-line fa-4x"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Pre-Investment Due Diligence</h5>
+      <p className="mb-4">Short Explanation on Pre-Investment Due Dilligence</p>
     </div>
   </div>
 </div>
@@ -145,44 +145,44 @@ class Service extends Component  {
 
 </div>
 </div>
-       <div class="row">
+       <div className="row">
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center bg-success text-white">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="far fa-object-ungroup fa-4x"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="text-white">Intellectual Property Violation Investigations</a></h5>
-      <p class="mb-4">Short explanation on IP</p>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center bg-success text-white">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="far fa-object-ungroup fa-4x"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Corporate Fraud and Theft Investigation</h5>
+      <p className="mb-4">Short explanation on CFTI</p>
     </div>
   </div>
 </div>
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="fas fa-mobile-alt fa-4x grey-text"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="dark-grey-text" href="#">Executive Due Dilligence</a></h5>
-      <p class="text-muted mb-4">Short Explanation on EDD</p>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="fas fa-mobile-alt fa-4x grey-text"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Litigation Support</h5>
+      <p className="text-muted mb-4">Short Explanation on LS</p>
     </div>
   </div>
 </div>
 
-<div class="col-md-6 col-xl-3 mb-4">
-  <div class="card text-center deep-purple lighten-1 text-white">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="fas fa-chart-line fa-4x"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="text-white" >Pre-Investment Due Diligence</a></h5>
-      <p class="mb-4">Short Explanation on Pre-Investment Due Dilligence</p>
+<div className="col-md-6 col-xl-3 mb-4">
+  <div className="card text-center deep-purple lighten-1 text-white">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="fas fa-chart-line fa-4x"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Business Risk Mitigation</h5>
+      <p className="mb-4">Short Explanation on BRM</p>
     </div>
   </div>
 </div>
 
-<div class="col-md-6 col-xl-3">
-  <div class="card text-center">
-    <div class="card-body">
-      <p class="mt-4 pt-2"><i class="fas fa-bullhorn fa-4x grey-text"></i></p>
-      <h5 class="font-weight-normal my-4 py-2"><a class="dark-grey-text">Corporate Fraud and Theft Investigation</a></h5>
-      <p class="text-muted mb-4">Short Explanation on CPFI.</p>
+<div className="col-md-6 col-xl-3">
+  <div className="card text-center">
+    <div className="card-body">
+      <p className="mt-4 pt-2"><i className="fas fa-bullhorn fa-4x grey-text"></i></p>
+      <h5 className="font-weight-normal my-4 py-2">Board Advisory</h5>
+      <p className="text-muted mb-4">Short Explanation on BA.</p>
     </div>
   </div>
 </div>
