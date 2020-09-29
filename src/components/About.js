fc194d38cc0b455980e76fbb7ecf14251c169875
ankundn <ankundnisha0@gmail.com>
2020-09-29 15:41:41 +0300

    added three columns in a row

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../index.css";
 // import Footer from "../components/Footer";
-import {  MDBBtn, MDBContainer,MDBCard, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from "mdbreact";
+import {  MDBBtn, MDBContainer,MDBCard, MDBCardGroup, MDBCardTitle, MDBCardText, MDBCardBody, MDBCol } from "mdbreact";
 
 
 class About extends React.Component {
@@ -11,64 +11,10 @@ class About extends React.Component {
       
     
 <div>
-      {/* <MDBJumbotron fluid className="jumbo">
-      <MDBContainer>
-        <MDBRow>
-          <MDBCol>
-          <h2 className="h1 display-4 text-center">Corporate Technical Services</h2>
-            <p className="aboutcontent">
-           A subsidiary of Captiva Africa Ltd
-           (<a href="https://captivaafrica.com">Captiva Africa</a>), was
-           established in 2002 in Nairobi,Kenya
-            </p>
-            <p className="aboutcontent">
-              When we started off, our mision was to integrate innovate technology
-        in the provision of security solutions for the corporate client.
-            </p>
-            <p className="aboutcontent">
-           Our initial services comprised the automation and system integration of
-           traditional security monitoring and response options.
-        </p>
-            <hr className="my-2" />
-            <p>
-              It uses utility classes for typgraphy and spacing to space content out
-              within the larger container.
-            </p>
-            <div className="text-center">
-              <MDBBtn color="primary" href="/service">CTS SERVICES</MDBBtn>
-            </div>
-          
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </MDBJumbotron> */}
+    
   
   
-    {/* <MDBContainer>
-  <MDBRow>
-    <MDBCol md="4">
-      <p className="aboutcontent">
-      CTS was the first company in EA to offer work-flow solutions to
-           the security decision chain for corporates
-      </p>
-    </MDBCol>
-    <MDBCol md="4">
-      <p className ="aboutcontent">
-      CTS pioneered the use of Radio Frequency tag technology for large
-               stores and supermarkets in Kenya to help prevent losses from theft
-            by employees and customers
-      </p>
-    </MDBCol>
-    <MDBCol md="4">
-      <p className="aboutcontent">
-      CTS was the first local private company in East Africa to be
-               contracted by government to offer long term technical assistance
-          to the police and ministry of interior
-      </p>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-   */}
+    
 
 
 <div className="container z-depth-1 my-5 py-5">
@@ -114,52 +60,52 @@ class About extends React.Component {
   
 </div> */}
 
-
-
-
-<MDBContainer>
-  <MDBCard className="card-body" style={{ width: "22rem", marginTop: "1rem" }}>
-    <MDBCardText>
-    <p className="aboutcontent">
-      CTS was the first company in EA to offer work-flow solutions to
+<MDBContainer className="aboutctscards">
+  <MDBCardGroup deck>
+    <MDBCard >
+      <MDBCardBody className="aboutcards">
+      
+        <MDBCardText className="text-center"  >
+        
+      CTS was the first company in East Africa to offer work-flow solutions to
            the security decision chain for corporates
-      </p>
-    </MDBCardText>
-    <div className="flex-row">
+      
+        </MDBCardText>
+       
+      </MDBCardBody>
+    </MDBCard>
 
-    </div>
-  </MDBCard>
-  <MDBCard className="card-body-two" style={{ width: "22rem", marginTop: "1rem" }}>
-    <MDBCardText>
-    <p className ="aboutcontent">
+
+    <MDBCard>
+      <MDBCardBody className="aboutcards">
+        
+        <MDBCardText className="text-center">
+    
       CTS pioneered the use of Radio Frequency tag technology for large
                stores and supermarkets in Kenya to help prevent losses from theft
             by employees and customers
-      </p>
-    </MDBCardText>
-    <div className="flex-row">
-
-    </div>
-  </MDBCard>
-  <MDBCard className="card-body-three" style={{ width: "22rem", marginTop: "1rem" }}>
-    <MDBCardText>
-    <p className="aboutcontent">
+      
+        </MDBCardText>
+        
+      </MDBCardBody>
+    </MDBCard>
+    
+    <MDBCard>
+      <MDBCardBody className="aboutcards">
+    
+        <MDBCardText className="text-center">
+        
       CTS was the first local private company in East Africa to be
                contracted by government to offer long term technical assistance
           to the police and ministry of interior
-      </p>
-    </MDBCardText>
-    <div className="flex-row">
-
-    </div>
-  </MDBCard>
+      
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+  </MDBCardGroup>
 </MDBContainer>
-
 </div>
-
   </section>
-  
-
 </div>
     </div>
     );
