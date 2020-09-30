4ffbbd63f0cdc4f09e670f2840de27cadc7fcbd8
ankundn <ankundnisha0@gmail.com>
2020-09-30 11:16:25 +0300

    Added the executive due diligence services offered

--- src/components/ExecutiveDueDilligence.js
+++ src/components/ExecutiveDueDilligence.js
@@ -4,58 +4,103 @@ import React from "react";
 const ExecutiveDueDilligence = () => {
   return (
    
-     <div class="container z-depth-1 my-5 py-5">
+     <div className="container z-depth-1 my-5 py-5">
 
+  <h6 className="font-weight-bold text-center grey-text text-uppercase small mb-4">Services</h6>
+  <h2 className="font-weight-bold text-center dark-grey-text pb-2">Executive Due Dilligence Services</h2>
+  <hr className="w-header my-4" />
+  <p className="lead text-center text-muted pt-2 mb-5"> We conduct due
+           diligence, on behalf of clients, on executive level staff or
+           candidates, as well as on key persons at prospective investee
+           companies or partners.</p>
+           <p className="lead text-center text-muted pt-2 mb-5">Our executive due diligence investigations may
+       be tailored to cover the following areas:</p>
 
+  <div className="row d-flex justify-content-center">
+  <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-secondary pt-4 mb-4">Confirmation of employment history</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Confirmation of employment history</p>
+      </div>
 
-  
-  {/* <style>
-    .border-top {
-      border-top-width: 2px !important;
-    }
-  </style> */}
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Confirmation of academic and professional license qualifications</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Confirmation of academic and professional license qualifications</p>
+      </div>
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">History of criminal and civil litigation</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on History of criminal and civil litigation</p>
+      </div>
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Misrepresentations in their backgrounds</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Misrepresentations in their backgrounds</p>
+      </div>
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Reputation Risks</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Reputation Risks</p>
+      </div>
 
-  <h6 class="font-weight-bold text-center grey-text text-uppercase small mb-4">Services</h6>
-  <h3 class="font-weight-bold text-center dark-grey-text pb-2">Our Services</h3>
-  <hr class="w-header my-4" />
-  <p class="lead text-center text-muted pt-2 mb-5">Join thousands of satisfied customers using our template
-    globally.</p>
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Politically exposed persons (PEPs)</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Politically exposed persons (PEPs)</p>
+      </div> 
 
-  <div class="row d-flex justify-content-center">
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Client/Supplier relationships</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Client/Supplier relationships</p>
+      </div> 
+      
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Extensive reference interviews</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Extensive reference interviews</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Interpol Search</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Interpol Search</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Source of Wealth</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on source of wealth</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Conflict of Interest</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on conflict of interesrt</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Pending Litigation</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Pending Litigation</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Involvement in other businesses</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Involvement in other businesses</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Credit Bureau Reference Checks</h5>
+        <p className="text-muted mb-5 pb-md-3">Credit Bureau Reference Checks</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Links to Organized Crime</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Links to Organized Crime</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Verification of Credentials</h5>
+        <p className="text-muted mb-5 pb-md-3">Explamation on Verification of Credentials</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Debts and Solvency</h5>
+        <p className="text-muted mb-5 pb-md-3">Explamation on Debts and Solvency</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Judgements</h5>
+        <p className="text-muted mb-5 pb-md-3">Explamation on Judgements</p>
+      </div> 
+      <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Regulatory Violations</h5>
+        <p className="text-muted mb-5 pb-md-3">Explamation on regulatory Violations</p>
+      </div> 
+      
+    
+    <div className="w-100"></div>
 
-    <div class="col-md-6 col-lg-5 col-xl-4">
-      <h5 class="font-weight-normal border-top border-light pt-4 mb-4"><a class="dark-grey-text" href="#">Website
-          Design</a></h5>
-      <p class="text-muted mb-5 pb-md-3">Written enquire painful to offices forming it. Then so does over sent
-        dull. Likewise offended humour mrs
-        fat trifling answered. On ye position greatest so desirous enable performance based.</p>
-    </div>
-
-    <div class="col-md-6 col-lg-5 col-xl-4">
-      <h5 class="font-weight-normal border-top border-secondary pt-4 mb-4"><a class="dark-grey-text"
-          href="#">Application Development</a></h5>
-      <p class="text-muted mb-5 pb-md-3">Written enquire painful to offices forming it. Then so does over sent
-        dull. Likewise offended humour mrs
-        fat trifling answered. On ye position greatest so desirous enable performance based.</p>
-    </div>
-
-    <div class="w-100"></div>
-
-    <div class="col-md-6 col-lg-5 col-xl-4">
-      <h5 class="font-weight-normal border-top border-light pt-4 mb-4"><a class="dark-grey-text" href="#">Branding
-          Package</a></h5>
-      <p class="text-muted mb-5">Written enquire painful to offices forming it. Then so does over sent dull.
-        Likewise offended humour mrs
-        fat trifling answered. On ye position greatest so desirous enable performance based.</p>
-    </div>
-
-    <div class="col-md-6 col-lg-5 col-xl-4">
-      <h5 class="font-weight-normal border-top border-light pt-4 mb-4"><a class="dark-grey-text"
-          href="#">Advertisement</a></h5>
-      <p class="text-muted mb-5">Written enquire painful to offices forming it. Then so does over sent dull.
-        Likewise offended humour mrs
-        fat trifling answered. On ye position greatest so desirous enable performance based.</p>
-    </div>
 
   </div>
   </div>
