30c9b69dfce81f27ce9f2831fbfbae645403ba64
ankundn <ankundnisha0@gmail.com>
2020-09-30 11:59:13 +0300

    changed the format of the services listed (2 by 2)

--- src/components/ExecutiveDueDilligence.js
+++ src/components/ExecutiveDueDilligence.js
@@ -15,95 +15,127 @@ const ExecutiveDueDilligence = () => {
            companies or partners.</p>
            <p className="lead text-center text-muted pt-2 mb-5">Our executive due diligence investigations may
        be tailored to cover the following areas:</p>
-
+<section>
   <div className="row d-flex justify-content-center">
+
   <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-secondary pt-4 mb-4">Confirmation of employment history</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Confirmation of employment history</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Confirmation of employment history</p>
       </div>
 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Confirmation of academic and professional license qualifications</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Confirmation of academic and professional license qualifications</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Confirmation of academic and professional license qualifications</p>
       </div>
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">History of criminal and civil litigation</h5>
+
+    <div className="w-100"></div>
+
+    <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">History of criminal and civil litigation</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on History of criminal and civil litigation</p>
       </div>
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Misrepresentations in their backgrounds</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Misrepresentations in their backgrounds</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Misrepresentations in their backgrounds</p>
       </div>
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Reputation Risks</h5>
+
+  </div>
+  
+
+
+  <div className="row d-flex justify-content-center">
+
+  <div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Reputation Risks</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Reputation Risks</p>
       </div>
 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Politically exposed persons (PEPs)</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Politically exposed persons (PEPs)</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Politically exposed persons (PEPs)</p>
       </div> 
 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Client/Supplier relationships</h5>
+<div className="w-100"></div>
+
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Client/Supplier relationships</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Client/Supplier relationships</p>
       </div> 
       
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Extensive reference interviews</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Extensive reference interviews</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Extensive reference interviews</p>
       </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Interpol Search</h5>
+
+</div>
+
+
+
+
+<div className="row d-flex justify-content-center">
+
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Interpol Search</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Interpol Search</p>
       </div> 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Source of Wealth</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Source of Wealth</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on source of wealth</p>
       </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Conflict of Interest</h5>
+
+<div className="w-100"></div>
+
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Conflict of Interest</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on conflict of interesrt</p>
       </div> 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Pending Litigation</h5>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Pending Litigation</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Pending Litigation</p>
       </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Involvement in other businesses</h5>
-        <p className="text-muted mb-5 pb-md-3">Explanation on Involvement in other businesses</p>
-      </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Credit Bureau Reference Checks</h5>
-        <p className="text-muted mb-5 pb-md-3">Credit Bureau Reference Checks</p>
-      </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Links to Organized Crime</h5>
+</div>
+
+
+
+<div className="row d-flex justify-content-center">
+
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Links to Organized Crime</h5>
         <p className="text-muted mb-5 pb-md-3">Explanation on Links to Organized Crime</p>
       </div> 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Verification of Credentials</h5>
-        <p className="text-muted mb-5 pb-md-3">Explamation on Verification of Credentials</p>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Verification of Credentials</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Verification of Credentials</p>
+      </div> 
+
+<div className="w-100"></div>
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Debts and Solvency</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Debts and Solvency</p>
       </div> 
       <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Debts and Solvency</h5>
-        <p className="text-muted mb-5 pb-md-3">Explamation on Debts and Solvency</p>
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Judgements</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on Judgements</p>
       </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Judgements</h5>
-        <p className="text-muted mb-5 pb-md-3">Explamation on Judgements</p>
+
+</div>
+
+<div className="row d-flex justify-content-center">
+
+<div className="col-md-6 col-lg-5 col-xl-4">
+        <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Regulatory Violations</h5>
+        <p className="text-muted mb-5 pb-md-3">Explanation on regulatory Violations</p>
       </div> 
-      <div className="col-md-6 col-lg-5 col-xl-4">
-        <h5 className="font-weight-normal border-top border-light pt-4 mb-4">Regulatory Violations</h5>
-        <p className="text-muted mb-5 pb-md-3">Explamation on regulatory Violations</p>
-      </div> 
-      
-    
-    <div className="w-100"></div>
+
+<div className="w-100"></div>
 
 
-  </div>
-  </div>
+</div>
+</section>
+
+
+</div>
+  
 
 
 
