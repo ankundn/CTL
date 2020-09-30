2bb0362c25f39c199ebc5187843640cec7e77928
ankundn <ankundnisha0@gmail.com>
2020-09-30 11:16:06 +0300

    Changed the header font

--- src/components/Service.js
+++ src/components/Service.js
@@ -80,115 +80,13 @@ import React, { Component } from 'react';
 
 
 class Service extends Component  {
-  // state = {
-  //   collapseID: ""
-  // }
-  
-  // toggleCollapse = collapseID => () => {
-  //   this.setState(prevState => ({
-  //     collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-  //   }));
-  // }
+
   render () {
 
     return (
       
       <div>
   
-    {/* <h1 className="font-weight-bold text-center dark-grey-text pb-2">Our Services</h1>
-    <hr />
-       <p className="servicecontent">
-             Our expansion into provision of advisory services was a natural
-             progression from security hardware and system integration.
-        </p> */}
-
-
-
-        {/* <div className="servicerow"> 
-<div className="row">
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center bg-success text-white">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="far fa-object-ungroup fa-4x"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Intellectual Property Violation Investigations</h5>
-      
-      <p className="mb-4">Short explanation on IP
-      </p>
-    
-    </div>
-  </div>
-</div>
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="fas fa-mobile-alt fa-4x grey-text"></i></p>
-      <h5 className="font-weight-normal my-4 py-2"><a className="dark-grey-text" href="/executiveduediligence">Executive Due Dilligence</a></h5>
-      
-      <p className="text-muted mb-4">Click for more information</p>
-    </div>
-  </div>
-</div>
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center deep-purple lighten-1 text-white">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="fas fa-chart-line fa-4x"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Pre-Investment Due Diligence</h5>
-      <p className="mb-4">Short Explanation on Pre-Investment Due Dilligence</p>
-    </div>
-  </div>
-</div>
-
-
-
-</div>
-</div>
-       <div className="row">
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center bg-success text-white">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="far fa-object-ungroup fa-4x"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Corporate Fraud and Theft Investigation</h5>
-      <p className="mb-4">Short explanation on CFTI</p>
-    </div>
-  </div>
-</div>
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="fas fa-mobile-alt fa-4x grey-text"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Litigation Support</h5>
-      <p className="text-muted mb-4">Short Explanation on LS</p>
-    </div>
-  </div>
-</div>
-
-<div className="col-md-6 col-xl-3 mb-4">
-  <div className="card text-center deep-purple lighten-1 text-white">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="fas fa-chart-line fa-4x"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Business Risk Mitigation</h5>
-      <p className="mb-4">Short Explanation on BRM</p>
-    </div>
-  </div>
-</div>
-
-<div className="col-md-6 col-xl-3">
-  <div className="card text-center">
-    <div className="card-body">
-      <p className="mt-4 pt-2"><i className="fas fa-bullhorn fa-4x grey-text"></i></p>
-      <h5 className="font-weight-normal my-4 py-2">Board Advisory</h5>
-      <p className="text-muted mb-4">Short Explanation on BA.</p>
-    </div>
-  </div>
-</div>
-
-</div> */}
-
 <div className="container z-depth-1 my-5 py-5">
 
   
@@ -196,10 +94,8 @@ class Service extends Component  {
     
     
 
-    {/* <h6 class="font-weight-bold text-center grey-text text-uppercase small mb-4">Services</h6> */}
-    <h2 className="h1-responsive font-weight-bold text-center my-5">
-      Our Services
-      </h2>
+    <h6 class="font-weight-bold text-center grey-text text-uppercase small mb-4">Services</h6>
+    <h2 class="font-weight-bold text-center dark-grey-text pb-2">Our Services</h2>
     <hr className="w-header my-4" />
     <p className="lead text-center text-muted pt-2 mb-5">Our expansion into provision of advisory services was a natural
              progression from security hardware and system integration.</p>
