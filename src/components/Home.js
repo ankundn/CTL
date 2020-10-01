9bf030489ca684c3f6036f80f2aecda490d7cde7
ankundn <ankundnisha0@gmail.com>
2020-10-01 17:09:13 +0300

    Placed the text in the middle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { MDBBtn } from "mdbreact";
+// import Footer from "../components/Footer";
 
 
 const Home = () => {
   return (
     
     <div className="child">
-      <div><h2 className="h1 display-4 text-center">Corporate Technical Services</h2></div>
+
+      <div className="hometext">
+        <h2 className="h1-responsive font-weight-bold text-center ">
+      Corporate Technical Services 
+      </h2>
+      </div>
 
     <div className="homebutton">
               <MDBBtn color="primary" href="/about">ABOUT US</MDBBtn>
             </div>
+
+            {/* <Footer /> */}
       </div>
   
 
