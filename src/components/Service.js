5280fa86d13b55be977e606ca4a1655248664d73
ankundn <ankundnisha0@gmail.com>
2020-10-05 13:46:52 +0300

    explaining rhe services inn depth

--- src/components/Service.js
+++ src/components/Service.js
@@ -28,24 +28,32 @@ class Service extends Component  {
 
       <div className="col-md-6 col-lg-5 col-xl-4">
         <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Executive Due Diligence</h5>
-        <p className="text-muted mb-5 pb-md-3">Explanation on EDD add link to executiveduediligence</p>
+        <p className="text-muted mb-5 pb-md-3">Majority of corporate crime does not originate from the shop floor but from the boardroom.
+        Executive Due Diligence is therefore a worthwhile investment of resources compared to what is at stake.
+       
+        </p>
+       
       </div>
 
       <div className="col-md-6 col-lg-5 col-xl-4">
         <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Intellectual Property Violation Investigations</h5>
-        <p className="text-muted mb-5 pb-md-3">Explanation on IPVI</p>
+        <p className="text-muted mb-5 pb-md-3">Identifying piracy of clients intellectual property assets as well as identifying
+        the organizations behind theft and distribution of their counterfeited and pirated intellectual property to negate these threats.
+        </p>
       </div>
 
       <div className="w-100"></div>
 
       <div className="col-md-6 col-lg-5 col-xl-4">
         <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Pre-Investment Due Diligence</h5>
-        <p className="text-muted mb-5">Explanation on PIDD.</p>
+        <p className="text-muted mb-5">Detailed investigation that a potential investor carries out on a target business before  completing
+        preliminary negotiations with it's owners.
+        </p>
       </div>
 
       <div className="col-md-6 col-lg-5 col-xl-4">
         <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Corporate Fraud and Theft Investigation</h5>
-        <p className="text-muted mb-5">Explanation on CFTI</p>
+        <p className="text-muted mb-5">We conduct thorough investigation of a corporation or business in order to uncover wrong doing committed by management, employees or third parties. </p>
       </div>
 
     </div>
@@ -56,12 +64,12 @@ class Service extends Component  {
 
 <div className="col-md-6 col-lg-5 col-xl-4">
   <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Litigation Support</h5>
-  <p className="text-muted mb-5 pb-md-3">Explanation on LS</p>
+  <p className="text-muted mb-5 pb-md-3">We provide consultation and support services to attorneys or others in regard to current and pending cases.</p>
 </div>
 
 <div className="col-md-6 col-lg-5 col-xl-4">
   <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Business Risk Mitigation</h5>
-  <p className="text-muted mb-5 pb-md-3">Explanation on BRM</p>
+  <p className="text-muted mb-5 pb-md-3">We create a strategy to prepare for and reduce the negative effects of threats and business continuity.</p>
 </div>
 
 <div className="w-100"></div>
@@ -69,7 +77,7 @@ class Service extends Component  {
 <div className="col-md-6 col-lg-5 col-xl-4">
   <h5 className="font-weight-normal border-top border-primary pt-4 mb-4">Board advisory in mergers and
  acquisitions</h5>
-  <p className="text-muted mb-5">Explanation on BA.</p>
+  <p className="text-muted mb-5">We offer advice to business and corporations through mergers and acquisitions.</p>
 </div>
 </div>
 
